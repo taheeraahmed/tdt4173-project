84c79de3b25532491854bafa4239cd78353e3f54
Taheera Ahmed <taheera@hotmail.com>
2023-10-27 13:58:28 +0200

    Training more models

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
-import warnings
 from models.lin_reg import lin_reg
 from models.random_forest import random_forest
-from sklearn.model_selection import train_test_split
+from models.decision_tree import decision_tree
 from data_preprocess import data_preprocess, get_training_data
 import logging
 from utils.set_up import set_up
@@ -16,17 +15,18 @@ def main():
     X = X.drop(columns=['time', 'date_calc'])
     logger.info('Done with preprocessing data')
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    numeric_features = X_train.select_dtypes(include=['float32']).columns.tolist()
-    categorical_features = X_train.select_dtypes(include=['object']).columns.tolist()
+    numeric_features = X.select_dtypes(include=['float32']).columns.tolist()
+    categorical_features = X.select_dtypes(include=['object']).columns.tolist()
     logger.info('The training is beginning')
 
     logger.info('Linear regression')
-    lin_reg(numeric_features, categorical_features, X_train, y_train)
+    lin_reg(numeric_features, categorical_features, X, y)
 
-    #logger.info('Random forest regression')
-    #random_forest(numeric_features, categorical_features, X_train, y_train)
+    logger.info('Decision tree')
+    decision_tree(numeric_features, categorical_features,X, y)
+    
+    logger.info('Random forest regression')
+    random_forest(numeric_features, categorical_features, X, y)
 
 
 if __name__ == "__main__":
