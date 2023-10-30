a273748b381923522a0e1f89a9fd4e344b2793bb
Taheera Ahmed <taheera@hotmail.com>
2023-10-30 22:52:09 +0100

    Removed logging of warnings

--- utils/set_up.py
+++ utils/set_up.py
@@ -4,8 +4,6 @@ import warnings
 import mlflow
 import random
 
-
-
 def set_up():
     result = pyfiglet.figlet_format("Gosling slayers", font = "slant"  ) 
     print(result) 
@@ -14,12 +12,12 @@ def set_up():
     LOG_FILE = "log_file.txt"
     WARNINGS_TO_SUPPRESS = [
     ("ignore", UserWarning, "_distutils_hack"),
-    ("ignore", FutureWarning, "mlflow.data.digest_utils")
+    ("ignore", FutureWarning, "mlflow.data.digest_utils"),
+    ("ignore", FutureWarning, "sklearn.preprocessing._encoders")
     ]
 
     for action, category, module in WARNINGS_TO_SUPPRESS:
         warnings.filterwarnings(action, category=category, module=module)
-
 
     logging.basicConfig(level=logging.INFO, 
                     format='[%(levelname)s] %(asctime)s - %(message)s',
@@ -28,3 +26,4 @@ def set_up():
                         logging.StreamHandler()
                     ])
     mlflow.autolog()
+    logging.getLogger('mlflow').setLevel(logging.ERROR)
