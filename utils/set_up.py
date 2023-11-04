624dfd5167a2c7e9cce4c9486806dac2a90b811f
Taheera Ahmed <taheera@hotmail.com>
2023-11-04 12:40:16 +0100

    Add handlings of warnings

--- utils/set_up.py
+++ utils/set_up.py
@@ -11,14 +11,16 @@ def set_up():
     random.seed(10)
     LOG_FILE = "log_file.txt"
     WARNINGS_TO_SUPPRESS = [
-    ("ignore", UserWarning, "_distutils_hack"),
-    ("ignore", FutureWarning, "mlflow.data.digest_utils"),
-    ("ignore", FutureWarning, "sklearn.preprocessing._encoders")
+        ("ignore", UserWarning, "_distutils_hack"),
+        ("ignore", FutureWarning, "mlflow.data.digest_utils"),
+        ("ignore", FutureWarning, "sklearn.preprocessing._encoders"),
+        ("ignore", UserWarning, "optional dependency `torch` is not available")
     ]
 
     for action, category, module in WARNINGS_TO_SUPPRESS:
         warnings.filterwarnings(action, category=category, module=module)
-
+    warnings.filterwarnings("ignore", category=UserWarning)
+    
     logging.basicConfig(level=logging.INFO, 
                     format='[%(levelname)s] %(asctime)s - %(message)s',
                     handlers=[
