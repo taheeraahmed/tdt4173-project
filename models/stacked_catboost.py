9177536f979139aca7e2be8a6992de5d0a1fa915
Taheera Ahmed <taheera@hotmail.com>
2023-11-06 15:07:33 +0100

    Refactor variable names and update imputer strategy

--- models/stacked_catboost.py
+++ models/stacked_catboost.py
@@ -14,9 +14,9 @@ def stacked_catboost(model_name='stacked-catboost'):
     logger.info('Processing data')
     data_a, data_b, data_c = load_data()
 
-    X_train_a, targets_a = get_train_targets(data_a)
-    X_train_b, targets_b = get_train_targets(data_b)
-    X_train_c, targets_c = get_train_targets(data_c)
+    X_train_a, y_a = get_train_targets(data_a)
+    X_train_b, y_b = get_train_targets(data_b)
+    X_train_c, y_c = get_train_targets(data_c)
 
     X_test_a, X_test_b, X_test_c = get_test_data()
 
@@ -28,21 +28,18 @@ def stacked_catboost(model_name='stacked-catboost'):
     data_process_pipeline = Pipeline([
         ('add_month', FeatureAdder()),
         ('drop_cols', ColumnDropper(drop_cols=drop_cols)),
-        ('imputer', SimpleImputer(missing_values=np.nan, strategy='constant', fill_value=0)),
+        ('imputer', SimpleImputer(missing_values=np.nan, strategy='mean', fill_value=0)),
     ])
+    
     run_name = generate_run_name()
 
     logger.info(f'Model name: {model_name}')
 
     # Define base models
     base_models = [
-        # ('cat_boost1', cb.CatBoostRegressor(random_state=1, silent=True)),
-        # ('cat_boost2', cb.CatBoostRegressor(random_state=2, silent=True)),
-        # ('cat_boost3', cb.CatBoostRegressor(random_state=3, silent=True)),
-        # ('cat_boost4', cb.CatBoostRegressor(random_state=4, silent=True)),
-        ('cat_boost1', cb.CatBoostRegressor(random_state=42, silent=True, border_count=86, depth=9, iterations=384, l2_leaf_reg=2.1607264050691626, learning_rate=0.023800792606525824)),
-        ('cat_boost2', cb.CatBoostRegressor(random_state=42, silent=True, border_count=81, depth=8, iterations=704, l2_leaf_reg=9.448753109694545, learning_rate=0.01698158072074776)),
-        ('cat_boost3', cb.CatBoostRegressor(random_state=42, silent=True, border_count=81, depth=8, iterations=704, l2_leaf_reg=9.448753109694545, learning_rate=0.01698158072074776)),
+        ('cat_boost1', cb.CatBoostRegressor(random_state=42, silent=True)),
+        ('cat_boost2', cb.CatBoostRegressor(random_state=42, silent=True)),
+        ('cat_boost3', cb.CatBoostRegressor(random_state=42, silent=True)),
         ('cat_boost4', cb.CatBoostRegressor(random_state=42, silent=True))
     ]
 
@@ -52,22 +49,21 @@ def stacked_catboost(model_name='stacked-catboost'):
     # Create the stacking regressor
     stacked_model = StackingRegressor(estimators=base_models, final_estimator=meta_learner)
 
-    #NOTE: can instead of using the sacked model just run a single model below:
     whole_model_pipeline = Pipeline([
         ('data_process', data_process_pipeline),
         ('stacked_model', stacked_model)
     ])
 
     logger.info("training location A model")
-    whole_model_pipeline.fit(X_train_a, targets_a)
+    whole_model_pipeline.fit(X_train_a, y_a)
     pred_a = whole_model_pipeline.predict(X_test_a.drop(columns=["id", "prediction", "location"]))
 
     logger.info("training location B model")
-    whole_model_pipeline.fit(X_train_b, targets_b)
+    whole_model_pipeline.fit(X_train_b, y_b)
     pred_b = whole_model_pipeline.predict(X_test_b.drop(columns=["id", "prediction", "location"]))
 
     logger.info("training location C model")
-    whole_model_pipeline.fit(X_train_c, targets_c)
+    whole_model_pipeline.fit(X_train_c, y_c)
     pred_c = whole_model_pipeline.predict(X_test_c.drop(columns=["id", "prediction", "location"]))
 
     prepare_submission(X_test_a, X_test_b, X_test_c, pred_a, pred_b, pred_c, run_name=run_name)
