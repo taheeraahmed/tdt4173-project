d2b76b95617e905f21b402f6eec98ee03f26a665
Andrea Stallvik <grimsdatter@gmail.com>
2023-11-07 17:22:01 +0100

    fixed bug so that time column is not deleted by rolling averages calculation

--- functions.py
+++ functions.py
@@ -167,7 +167,7 @@ def get_hourly_mean(df):
 def rolling_average(df, window_size=24,features=['clear_sky_energy_1h:J','clear_sky_rad:W', 'direct_rad:W', 'direct_rad_1h:J', 'diffuse_rad:W', 'diffuse_rad_1h:J', 'total_cloud_cover:p', 'sun_elevation:d']):
     # Ensure the 'time' column is datetime and set as index
     df['time'] = pd.to_datetime(df['time'])
-    df.set_index('time', inplace=True)
+    df.set_index('time', inplace=True, drop=False)
     df.sort_index(inplace=True)
 
     # Calculate rolling averages for the specified features
