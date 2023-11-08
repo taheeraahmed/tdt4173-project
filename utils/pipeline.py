1f98d8afa63e3547ab4e23bd72e8de9753ebffe4
Taheera Ahmed <taheera@hotmail.com>
2023-11-08 09:22:14 +0100

    Remove pipeline logging functionality

--- utils/pipeline.py
+++
@@ -1,7 +0,0 @@
-import mlflow
-
-def run_pipeline_and_log(pipeline, X_train, y_train, X_test, location):
-    with mlflow.start_run(run_name=f"pipeline_{location}"):
-        pipeline.fit(X_train, y_train)
-        predictions = pipeline.predict(X_test.drop(columns=["id", "prediction", "location"]))
-    return predictions
