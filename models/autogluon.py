df245b6c59c75b9d868b0a5ef544b559662310db
Taheera Ahmed <taheera@hotmail.com>
2023-11-08 13:21:34 +0100

    Refactored feature engineering and added model path to save location

--- models/autogluon.py
+++ models/autogluon.py
@@ -2,8 +2,7 @@ from autogluon.tabular import TabularPredictor
 import logging 
 from utils.generate_run_name import generate_run_name
 from utils.read_data import load_data, get_test_data, prepare_submission
-import numpy as np
-import pandas as pd
+from utils.log_model import check_create_directory
 
 def autogluon(model_name = 'autogluon-with-more-feature-eng'):
     logger = logging.getLogger()
@@ -12,15 +11,14 @@ def autogluon(model_name = 'autogluon-with-more-feature-eng'):
 
     logger.info('Processing data')
 
-    data_a, data_b, data_c = load_data(mean=True, remove_out=True, roll_avg=True)
-    X_test_a, X_test_b, X_test_c = get_test_data(mean=True, roll_avg=True)
+    data_a, data_b, data_c = load_data(mean=True, remove_out=True, roll_avg=True, cust_feat=True)
+    X_test_a, X_test_b, X_test_c = get_test_data(mean=True, roll_avg=True, cust_feat=True)
 
     logger.info('Done processing data')
 
     # Specify the column name that contains the target variable to predict
     label = 'pv_measurement'
 
-    logger.info('Done processing data')
 
     # Assuming 'logger' is already defined and 'label' and 'run_name' are defined elsewhere
     data_frames = [('A', data_a, X_test_a), ('B', data_b, X_test_b), ('C', data_c, X_test_c)]
@@ -28,17 +26,16 @@ def autogluon(model_name = 'autogluon-with-more-feature-eng'):
     predictions = {}
 
     for location, data, X_test in data_frames:
-        # Train the model
+        # train the model
         logger.info(f'Training for location {location}')
-        model = TabularPredictor(label=label, problem_type='regression').fit(data)
+        # the base directory where you want to check/create
+        base_path = 'autogluon'  
+        model_path = check_create_directory(base_path, run_name)
+        model = TabularPredictor(label=label, problem_type='regression', path=model_path).fit(data)
         models[location] = model
-
-        # Save the model
-        model_path = f'autogluon/{location.lower()}'
-        model.save(model_path)
         logger.info(f'Model saved for location {location} at {model_path}')
-
-        # Make predictions
+        
+        # make predictions
         predictions[location] = model.predict(X_test.drop(columns=["id", "prediction", "location"]))
 
     # Prepare submission
