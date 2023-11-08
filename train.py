06ec3d81197e7f502b7f4e5a90286bb3e014a225
Taheera Ahmed <taheera@hotmail.com>
2023-11-08 14:42:28 +0100

    Replace autogluon with stacked_catboost in train.py

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from models.autogluon import autogluon
+from models.stacked_catboost import stacked_catboost
 import logging
 from utils.set_up import set_up
 
@@ -6,8 +6,9 @@ def main():
     set_up()
     logger = logging.getLogger()
     logger.info('The training is beginning')
+
+    stacked_catboost()
     
-    autogluon()
 
 if __name__ == "__main__":
     main()
