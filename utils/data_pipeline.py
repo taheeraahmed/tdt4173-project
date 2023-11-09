ad92ef67ac30c06183f9f757b7e70f94b26ee59b
taheeraahmed <taheera@hotmail.com>
2023-11-09 18:52:53 +0100

    Added transforms

--- utils/data_pipeline.py
+++ utils/data_pipeline.py
@@ -1,5 +1,6 @@
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
+from utils.feature_fixing import get_hourly, get_hourly_mean, cyclic_encoding, add_custom_features,remove_ouliers, rolling_average, normalize
 import pandas as pd
 
 """
@@ -15,52 +16,17 @@ data_process_pipeline = Pipeline([
 class FeatureAdder(BaseEstimator, TransformerMixin):
     """Adds features."""
 
-    def __init__(self, drop_cols = [], is_test = False):
-        self.drop_cols = drop_cols
-
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
         X_copy = X.copy()
-
-        # add moth
-        X_copy['month'] = X_copy['time'].apply(lambda x: x.month)
-        # add hour
-        X_copy['hour'] = X_copy['time'].apply(lambda x: x.hour)
-
-        # -- additive effects:
-        #X_copy["sun_rad_1"] = (X_copy['sun_azimuth:d'] * X_copy['direct_rad:W']) / 1000000
-        X_copy["sun_rad_2"] = (X_copy['sun_elevation:d'] * X_copy['direct_rad:W']) / 1000000
-        #X_copy["sun_wind_1"] = (X_copy['wind_speed_10m:ms'] * X_copy['direct_rad:W']) / 1000
-        X_copy["sun_wind_2"] = (X_copy['wind_speed_10m:ms'] * X_copy['diffuse_rad:W']) / 1000
-        X_copy["temp_sun"] = (X_copy['t_1000hPa:K'] * X_copy['sun_azimuth:d'])/1000
-        X_copy["rad_day_1"] = (X_copy['is_day:idx'] * X_copy['diffuse_rad:W']) / 1000
-        X_copy['mult_coulds'] = (X_copy['clear_sky_rad:W'] * X_copy['cloud_base_agl:m']) / 100000
-
-        # rolling averages
-        X_copy = self.rolling_average(X_copy)
+        X_copy = cyclic_encoding(X.copy)
+        X_copy = add_custom_features(X_copy)
+        X_copy = remove_ouliers(X_copy)
+        X_copy = rolling_average(X_copy)
         return X_copy
     
-    def rolling_average(self, df, window_size=24,features=['clear_sky_energy_1h:J','clear_sky_rad:W', 'direct_rad:W', 'direct_rad_1h:J', 'diffuse_rad:W', 'diffuse_rad_1h:J', 'total_cloud_cover:p', 'sun_elevation:d']):
-        # Ensure the 'time' column is datetime and set as index
-
-        features = ['precip_5min:mm', 'rain_water:kgm2', 'prob_rime:p', 't_1000hPa:K', 'snow_water:kgm2', 'visibility:m', # just this 7 nov
-            'clear_sky_energy_1h:J','clear_sky_rad:W', 'direct_rad:W', 'direct_rad_1h:J', 'diffuse_rad:W', 'diffuse_rad_1h:J', 'total_cloud_cover:p', 'sun_elevation:d'] # added for 8 nov
-        
-        df['time'] = pd.to_datetime(df['time'])
-        df.set_index('time', inplace=True, drop=False)
-        df.sort_index(inplace=True)
-
-        # Calculate rolling averages for the specified features
-        for feature in features:
-            rolling_feature_name = f"{feature}_rolling_avg_{window_size}"
-            df[rolling_feature_name] = df[feature].rolling(window=window_size).mean()
-
-        # Handle missing data if necessary
-        df.fillna(method='bfill', inplace=True)  # Forward fill
-        return df
-
 class ColumnDropper(BaseEstimator, TransformerMixin):
     """Drops columns from the data."""
 
