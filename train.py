11af573dc50c913e302d124da1f4d7053d0a0bec
taheeraahmed <taheera@hotmail.com>
2023-11-10 21:34:37 +0100

    Removed unused stuff

--- train.py
+++ train.py
@@ -1,14 +1,11 @@
-from models.bayesian_search import param_search_bayes_xgboost
-import logging
 from utils.set_up import set_up
+import logging
 
 def main():
     set_up()
     logger = logging.getLogger()
     logger.info('The training is beginning')
 
-    param_search_bayes_xgboost()
-    
 
 if __name__ == "__main__":
     main()
