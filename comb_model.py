8b191bff411e51013632eb307179eec4b1efb817
Andrea Stallvik <grimsdatter@gmail.com>
2023-11-11 11:01:31 +0100

    run this on IDUN

--- comb_model.py
+++ comb_model.py
@@ -18,13 +18,13 @@ from xgboost import XGBRegressor
 # Suppress all FutureWarnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-data_a, data_b, data_c = load_data(mean=True, remove_out=True, roll_avg=True)
+data_a, data_b, data_c = load_data(mean_stats=True, remove_out=True, roll_avg=True)
 
 X_train_a, targets_a = get_train_targets(data_a)
 X_train_b, targets_b = get_train_targets(data_b)
 X_train_c, targets_c = get_train_targets(data_c)
 
-X_test_a, X_test_b, X_test_c = get_test_data(mean=True, roll_avg=True)
+X_test_a, X_test_b, X_test_c = get_test_data(mean_stats=True, roll_avg=True)
 
 
 drop_cols = ['time', 'elevation:m', 'fresh_snow_1h:cm', 'ceiling_height_agl:m', 'snow_density:kgm3', 
@@ -52,13 +52,13 @@ data_process_pipeline = Pipeline([
 
 
 base_modelsA = [
-    ('cat_boost1', cb.CatBoostRegressor(random_state=1, silent=True, objective="MAE", depth=10)), #andrea gjør søk
+    ('cat_boost1', cb.CatBoostRegressor(random_state=1, silent=True, objective="MAE", border_count=157, depth=13, iterations=828, l2_leaf_reg=7.677745179031975, learning_rate=0.012997359346271088)), #andrea gjør søk
     ('cat_boost2', cb.CatBoostRegressor(random_state=2, silent=True, depth=10)),
-    ('xgb_reg1', XGBRegressor(random_state=12, eval_metric="mae")), #Taheera gjør søk
+    ('xgb_reg1', XGBRegressor(random_state=12, eval_metric="mae", colsample_bytree=0.588602113426499, max_depth=12, n_estimators=500, reg_alpha=1e-09, reg_lambda=0.0001, xee=0.588602113426499, learning_rate=0.023222800065583988,gboost__subsample=0.4665774062657444)), #Taheera gjør søk
     ('xgb_reg2', XGBRegressor(random_state=42)),
     ('xgb_reg3', XGBRegressor(random_state=16, eval_metric="mae")),
     ('cat_boost3', cb.CatBoostRegressor(random_state=3, silent=True)),
-    ('cat_boost4', cb.CatBoostRegressor(random_state=32, silent=True, objective="MAE", depth=10)), #lagt til
+    #('cat_boost4', cb.CatBoostRegressor(random_state=32, silent=True, objective="MAE", depth=10)), #lagt til
 ]
 
 base_modelsB = [
@@ -117,4 +117,4 @@ pred_c = modelC_pipeline.predict(X_test_c.drop(columns=["id", "prediction", "loc
 submission = prepare_submission(X_test_a, X_test_b, X_test_c, pred_a, pred_b, pred_c)
 submission['prediction'] = submission['prediction'].apply(lambda x: 0 if x < 0.1 else x)
 
-submission.to_csv('submissions/10_nov_1930.csv', index=False)
+submission.to_csv('submissions/11_nov_1055.csv', index=False)
